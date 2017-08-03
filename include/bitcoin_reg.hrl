ae202e24cc918679e41bb4feb04779c82ed691c1
Da Few <1ofdafew@gmail.com>
2017-08-03 16:19:17 +0400

    Removed tnc

--- include/bitcoin_reg.hrl
+++ include/bitcoin_reg.hrl
@@ -2,7 +2,7 @@
 -define(record_to_tuplelist(Rec, Ref), lists:zip(record_info(fields, Rec),tl(tuple_to_list(Ref)))).
 -define(tuplelist_to_record(Rec, Ref), list_to_tuple([Rec|[V || {_,V} <- Ref]])).
 
--record (btc_reg, {uuid, btc_address, btc_tx_hash, eth_address, btc_sig, contact, referrer, tnc, created_at, updated_at}).
+-record (btc_reg, {uuid, btc_address, btc_tx_hash, eth_address, btc_sig, contact, referrer, created_at, updated_at}).
 -record (btc_user, {uuid, username, password, created_at, updated_at}).
 
 -define(DEBUG(Text), lager:log(debug, ?MODULE, "~p:~p: " ++ Text ++ "~n", [?MODULE, ?LINE])).
